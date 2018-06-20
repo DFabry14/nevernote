31cda977f5b4d7d4b3b5bf1eba57a43b0897cfe2
Dan Fabry <dfabry14@gmail.com>
2018-06-20 13:52:22 -0500

    initial files

--- server.js
+++ server.js
@@ -1,24 +1,56 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const db = require("./models")
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+//GET route to retrieve all current todos from database
+app.get("/api/todos", function (req, res) {
+  db.Todo
+    .find({})
+    .then(results => res.json(results))
+    .catch(err => {
+    console.log(err);
+    res.status(422).json(err);
+    });
+});
+
+app.post("/api/todo", (req, res) => {
+  const newTodo = {
+    todo: req.body.todo,
+    note: req.body.note,
+    deadline: req.body.deadline,
+    category: req.body.category
+  };
+  db.Todo
+    .create(newTodo)
+    .then(results => res.json(results))
+    .catch(err => {
+    console.log(err)
+    res.status(422).json(err);;
+    });
+});
+
 // Send every request to the React app that doesn't match another Express route
 // Define any API routes before this runs
-
-
-
-
 // This route should always be at the bottom of the routes list
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://test:test14@ds163510.mlab.com:63510/heroku_tl8bmc69");
+
+app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
